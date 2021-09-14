204f3de6dfe3261ad197436f46e91a92d792e374
Francisco <fvarelacid@gmail.com>
2021-09-14 17:52:37 +0100

    Update Normalization for Train and Test

--- models.py
+++ models.py
@@ -12,11 +12,13 @@ train_transform = transforms.Compose([transforms.Resize((224, 224)),
                                     transforms.RandomRotation(30),
                                     transforms.RandomHorizontalFlip(),
                                     transforms.ToTensor(),
-                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) ])
+                                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225])])
 
 test_transform = transforms.Compose([transforms.Resize(224, 224),
                                     transforms.ToTensor(),
-                                    transforms.Normalize((0.5), (0.5)) ])
+                                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225])])
 
 train_dataset = datasets.ImageFolder("data/train", transform=train_transform)
 test_dataset = datasets.ImageFolder("data/val", transform=test_transform)
