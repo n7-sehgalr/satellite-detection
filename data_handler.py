2f877741cf148f35c712b6d399c6f673d20908ef
Rajat Sehgal <sehgal.rajat1007@gmail.com>
2021-09-15 11:50:16 +0200

    Correct normalization values

--- data_handler.py
+++ data_handler.py
@@ -6,15 +6,17 @@ from sklearn.model_selection import train_test_split
 from torchvision import datasets, transforms
 
 
-train_transform = transforms.Compose([transforms.Resize(255),
+train_transform = transforms.Compose([transforms.Resize(224),
                                     transforms.RandomRotation(30),
                                     transforms.RandomHorizontalFlip(),
                                     transforms.ToTensor(),
-                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) ])
+                                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225])])
 
-test_transform = transforms.Compose([transforms.Resize(255),
+test_transform = transforms.Compose([transforms.Resize(224, 224),
                                     transforms.ToTensor(),
-                                    transforms.Normalize((0.5), (0.5)) ])
+                                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225])])
 
 train_dataset = datasets.ImageFolder("data/train", transform=train_transform)
 test_dataset = datasets.ImageFolder("data/val", transform=test_transform)
