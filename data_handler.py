02687cdb3bbe5c32d775844ee9d766017b40715c
Rajat Sehgal <sehgal.rajat1007@gmail.com>
2021-09-15 15:12:55 +0200

    Correct file paths

--- data_handler.py
+++ data_handler.py
@@ -8,10 +8,10 @@ from sklearn.model_selection import train_test_split
 from torchvision import datasets, transforms
 
 def splitting(path):
-    if os.path.exists(os.path.join(path, "train")):
-        pass
+    if os.path.exists("data/dataset_splits"):
+        return
     else:
-        splitfolders.ratio(path, output="dataset_splits", seed=1337, ratio=(.8, 0.1,0.1))
+        splitfolders.ratio(path, output="data/dataset_splits", seed=1337, ratio=(.8, 0.1,0.1))
 
 splitting("data/2750")
 
@@ -22,22 +22,23 @@ train_transform = transforms.Compose([transforms.Resize(224),
                                     transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                     std=[0.229, 0.224, 0.225])])
 
-test_transform = transforms.Compose([transforms.Resize(224, 224),
+val_transform = transforms.Compose([transforms.Resize(224, 224),
                                     transforms.ToTensor(),
                                     transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                     std=[0.229, 0.224, 0.225])])
 
-train_dataset = datasets.ImageFolder("data/train", transform=train_transform)
-test_dataset = datasets.ImageFolder("data/val", transform=test_transform)
-
+train_dataset = datasets.ImageFolder("data/dataset_splits/train", transform=train_transform)
+val_dataset = datasets.ImageFolder("data/dataset_splits/val", transform=val_transform)
+print(len(train_dataset), len(val_dataset))
 
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=16)
+test_loader = torch.utils.data.DataLoader(val_dataset, batch_size=16)
 
 train_iter = iter(train_loader)
 test_iter = iter(test_loader)
 
 images, labels = train_iter.next()
+print(images.shape, labels.shape)
 print(images[0], labels[0])
 print('Data DUELY HANDLED')
 
