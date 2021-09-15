8cadcb129ce2122eb3706316a13e431eebf561e3
Rajat Sehgal <sehgal.rajat1007@gmail.com>
2021-09-15 13:36:36 +0200

    Add split folders function

--- data_handler.py
+++ data_handler.py
@@ -1,10 +1,19 @@
 # from operator import index
 import pandas as pd
 import numpy as np
+import os
+import splitfolders
 import torch
 from sklearn.model_selection import train_test_split
 from torchvision import datasets, transforms
 
+def splitting(path):
+    if os.path.exists(os.path.join(path, "train")):
+        pass
+    else:
+        splitfolders.ratio(path, output="dataset_splits", seed=1337, ratio=(.8, 0.1,0.1))
+
+splitting("data/2750")
 
 train_transform = transforms.Compose([transforms.Resize(224),
                                     transforms.RandomRotation(30),
