9a18b2095553040aee97934ab03debb1a37142f3
Rajat Sehgal <sehgal.rajat1007@gmail.com>
2021-09-17 02:01:57 +0200

    Improved training script

--- train.py
+++ train.py
@@ -1,3 +1,6 @@
+import time
+import os
+import copy
 import numpy as np
 import torch 
 from torch import nn
@@ -9,61 +12,86 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 from torchsummary import summary
 
+def train_model(model, criterion, optimizer, dataloaders, device, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        
+        print('Epoch{}/{}'.format(epoch+1, num_epochs))
+        print('-'*10)
+
+        try:
+            for phase in ['train', 'val']:
+                if phase == 'train':
+                    model.train()
+                else:
+                    model.eval()
+                
+                running_loss = 0.0
+                running_corrects = 0.0
+
+                for inputs, labels in tqdm(dataloaders[phase]):
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+
+                    optimizer.zero_grad()
+
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = model(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        loss = criterion(outputs, labels)
+
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
+                        
+                    running_loss += loss.item()*inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
+                
+                epoch_loss = running_loss/dataset_sizes[phase]
+                epoch_acc = running_corrects.double()/dataset_sizes[phase]
+
+                print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+                if phase =='val' and epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    best_model_wts = copy.deepcopy(model.state_dict())
+                
+            print()
+            
+        except Exception as e:
+            with open('log.txt', 'a') as file:
+                file.write(str(e))
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    model.load_state_dict(best_model_wts)
+
+    return model
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 train_path = "data/dataset_splits/train"
 val_path = "data/dataset_splits/val"
 test_path = "data/dataset_splits/test"
 
 train_loader, val_loader, _ = dataset_loader(train_path, val_path, test_path)
+dataloaders = {'train': train_loader, 'val': val_loader}
+
+dataset_sizes = {x: len(os.listdir(f'data/dataset_splits/{x}')) for x in ['train', 'val']}
+
+model = model.to(device)
+print(summary(model, (3, 224, 224)))
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.fc.parameters(), lr=0.001, momentum=0.9)
 
-epochs = 5
-
-epoch_values = []
-loss_values = []
-valid_loss_values = []
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model = model.to(device)
-print(summary(model, (3, 224, 224)))
-
-running_loss = 0
-running_valid_loss = 0
-for e in tqdm(range(epochs)):
-    print(f"Epoch: {e+1}")
-
-    try:
-        for i, (inputs, labels) in tqdm(enumerate(iter(train_loader))):
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-            outputs = model(inputs)
-            optimizer.zero_grad()
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            # print statistics
-            running_loss += loss.item()
-            if i % 50 == 49:    # print every 50 mini-batches
-                print('[%d, %5d] Train loss: %.3f' % (e + 1, i + 1, running_loss / 50))
-                running_loss = 0.0
-
-        with torch.no_grad():
-            for i, (inputs, labels) in enumerate(iter(val_loader)):
-                inputs = inputs.to(device)
-                labels = labels.to(device)
-                outputs = model(inputs)
-                valid_loss = criterion(outputs, labels)
-                running_valid_loss += valid_loss.item()
-
-                if i % 50 == 49:    # print every 50 mini-batches
-                    print('[%d, %5d] Val loss: %.3f' % (e + 1, i + 1, running_valid_loss / 50))
-                    running_valid_loss = 0
-
-        epoch_values.append(e+1)    
-        loss_values.append(running_loss/50)
-        valid_loss_values.append(running_valid_loss/50)
-
-    except Exception as e:
-        with open('log.txt', 'a') as file:
-            file.write(str(e))
+model = train_model(model, criterion, optimizer, dataloaders, device, num_epochs=50)
+torch.save(model.state_dict(), 'trained_model.pth')
